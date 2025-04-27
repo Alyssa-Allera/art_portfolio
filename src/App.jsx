2aadcede7e8112c5f9d333635e97e512ee74c0b1
Alyssa-Allera <alyssa.allera.may@gmail.com>
2025-04-27 19:23:19 +0800

    Home page initial

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,13 @@ const router = createBrowserRouter([{
   path: '/',
   element: ''
 },
-{
-  path: '/home',
-  element: <NavBar />
-}
 ]);
 
 export default function App() {
   return (
     <>
       <div className="w-screen h-screen px-[10rem] bg-[#17153B] overflow-hidden">
+        <NavBar />
         <RouterProvider router={router}/>
       </div>
       </>
